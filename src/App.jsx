94fce2e9ecf1c933cc52b70edc3808cb7d02e468
suhitha <157236546+suhithasuhi17@users.noreply.github.com>
2025-03-13 21:36:37 +0530

    Signup_page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import './App.css'
 
 function App() {
-  
-
   return (
-    <div className="dashboard-container">
-     
-      <div className="sidebar">
-        <h2>My Dashboard</h2>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Settings</a></li>
-          <li><a href="#">Logout</a></li>
-        </ul>
-      </div>
-
-      <main className="main">
-        <h1>Welcome, User!</h1>
-        <p>Here's a quick overview of your activity.</p>
-      </main>
-    </div>
+    <div class="signup-container">
+    <h2>Create an Account</h2>
+    <form>
+        <div class="input">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Enter your username" />
+        </div>
+        <div class="input">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email" />
+        </div>
+        <div class="input">
+            <label for="password">Password</label>
+            <input type="password"   placeholder="Create a password"/> 
+        </div>
+        <div class="input">
+            <label >Confirm Password</label>
+            <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password" />
+        </div>
+        <button type="submit">Sign Up</button>
+        <div class="login">
+            Already have an account? <a href="#">Log In</a>
+        </div>
+    </form>
+</div>
   )
 }
 
